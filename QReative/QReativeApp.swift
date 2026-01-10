295bfe4177e16a2f5249d82943f4055992ebf5a6
Burak Ahmet Şentürk <b95senturk@gmail.com>
2026-01-10 12:30:40 +0300

    Initial Commit

---
+++ QReative/QReativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QReativeApp.swift
+//  QReative
+//
+//  Created by Burak Ahmet Şentürk on 10.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct QReativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
